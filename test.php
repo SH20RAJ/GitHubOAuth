7034e383a235d187b1b3153b6ccae670a0fd56c1
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-06-19 09:54:01 +0000

    Video made on it used test.php

--- test.php
+++ test.php
@@ -1,16 +1,56 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+include 'GitHubOAuth.php';
 
 require_once 'GitHubOAuth.php';
 
-$client_id = 'd9d2f82db231d388f024';
-$client_secret = 'eb60e47d2d303907224e2ca9aae8411c17894644';
-$redirect_uri = 'https://githuboauth.sh20raj.repl.co/';
+$client_id = '27a40c367652e783b3a6';
+$client_secret = '4749e1adb96668731da1f14df9ce6066a9757519';
+$redirect_uri = 'https://githuboauth.sh20raj.repl.co/test.php';
 
 $github_oauth = new GitHubOAuth($client_id, $client_secret, $redirect_uri);
 
-//$github_oauth->auth_code = 'ffcd70cf0a3fd76fc0e7';
-echo $access_token = $github_oauth->getAccessToken('bdfc6dfe0b81d60c1375');
+$authorization_url = $github_oauth->getAuthorizationUrl();
+
+// Display the login button or redirect the user to the authorization URL
+echo '<hr><a href="' . $authorization_url . '">Login with GitHub</a><hr>';
+
+
+
+if (isset($_GET['code'])) {
+    $auth_code = $_GET['code'];
+
+    $access_token = $github_oauth->getAccessToken($auth_code);
+
+    if ($access_token !== false) {
+        $_SESSION['access_token'] = $access_token;
+
+        // Redirect the user to the desired page
+        //header('Location: welcome.php');
+//exit;
+    } else {
+        //echo 'Failed to obtain access token';
+    }
+}
+
+
+if (isset($_SESSION['access_token'])) {
+    $access_token = $_SESSION['access_token'];
+
+    $user_data = $github_oauth->getAuthenticatedUser($access_token);
+
+    if ($user_data !== false) {
+        // Process user data as needed
+        echo 'User ID: ' . $user_data->id . '<br>';
+        echo 'Login: ' . $user_data->login . '<br>';
+      echo 'Name: ' . $user_data->name . '<br>';
+      echo 'Company: ' . $user_data->company . '<br>';
+      echo '<a href="logout.php">Logout</a>';
+        // ...
+    } else {
+        echo 'Failed to retrieve user data from GitHub';
+    }
+} else {
+   // echo 'Access token not found';
+}
+?>
